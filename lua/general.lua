13106681d090d3b292719f9db9535f998eea676b
Lana Dzyuban <lana.dzyuban@shopify.com>
2026-02-18 10:55:19 -0500

    jk and WQ

--- lua/general.lua
+++ lua/general.lua
@@ -211,6 +211,13 @@ vim.keymap.set('t', '<C-w>l', "<C-\\><C-n><C-w>l",{silent = true})
 vim.keymap.set('t', '<C-w>w', "<C-\\><C-n><C-w>w",{silent = true})
 vim.keymap.set('t', '<C-w><C-w>', "<C-\\><C-n><C-w><C-w>",{silent = true})
 
+-- Remap esc
+vim.keymap.set('i', 'jk', '<Esc>')
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+
 -- fff.nvim (fast file finder)
 vim.keymap.set("n", "<leader>ff", function() require('fff').find_files() end, { desc = "Find files (fff)" })
 vim.keymap.set("n", "<leader>fF", function() require('fff').find_in_git_root() end, { desc = "Find files in git root (fff)" })
