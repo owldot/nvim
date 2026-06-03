a6311a5b2a620615fcf5de6a41ac25b3cf9270f9
Lana Dzyuban <svitlana.dzyuban@gmail.com>
2026-06-03 19:55:57 +0200

    cleanup oil

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -167,16 +167,6 @@ extensions = {
         view_options = {
           show_hidden = true,
         },
-        keymaps = {
-          ["g?"] = "actions.show_help",
-          ["<CR>"] = "actions.select",
-          ["-"] = "actions.parent",
-          ["_"] = "actions.open_cwd",
-          ["`"] = "actions.cd",
-          ["gs"] = "actions.change_sort",
-          ["gx"] = "actions.open_external",
-          ["g."] = "actions.toggle_hidden",
-        },
       })
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
